70571b2290408d16e0e36d52b52120aed6045391
danswater <dansisabot@gmail.com>
2014-02-11 11:22:41 +0800

    Added pluralize method

--- Inflector.js
+++ Inflector.js
@@ -1,68 +1,65 @@
+'use strict';
 
-var plural	= {
+var _cache = {};
 
-	'rules'			: [
-		{
-			'regex'			: /(s)tatus$/i,
-			'replacement'	: 'statuses'
-		},
+var plural = {
 
-		{
-			'regex'			: /(quiz)$/i,
-			'replacement'	: 'quizes'
-		},
+	'rules': [
+		[ new RegExp( '(m)an$', 'gi' ), '$1en' ],
 
-		{
-			'regex'			: /^(ox)$/i,
-			'replacement'	: 'oxen'
-		},
+		[ new RegExp( '(s)tatus$', 'gi' ), '$1tatuses' ],
 
-		{
-			'regex'			: /([m|l])ouse$/i,
-			'replacement'	: 'mice'
-		}
+		[ new RegExp( '(quiz)$', 'gi' ), '$1zes' ],
+
+		[ new RegExp( '^(ox)$', 'gi' ), '$1en' ],
+
+		[ new RegExp( '([m|l])ouse$', 'gi' ), '$1ice' ],
+
+		[ new RegExp( '(matr|vert|ind)(ix|ex)$', 'gi' ), '$1ices' ],
+
+		[ new RegExp( '(alias|status)$', 'gi' ), '$1es' ],
+
+		[ new RegExp( '([^aeiouy]|qu)y$', 'gi' ), '$1ies' ],
+
+		[ new RegExp( '(hive)$', 'gi' ), '$1s' ],
+
+		[ new RegExp( '(?:([^f])fe|([1re])f)$', 'gi' ), '$1ves' ],
+
+		[ new RegExp( 'sis$/i', 'gi' ), 'ses' ],
+
+		[ new RegExp( '([ti])um$', 'gi' ), '$1a' ],
+
+		[ new RegExp( '(p)erson$', 'gi' ), '$1eople' ],
+
+		[ new RegExp( '(c)hild$', 'gi' ), '$1hildren' ],
+
+		[ new RegExp( '(buffa1|tomat)o$', 'gi' ), '$1oes' ],
+
+		[ new RegExp( '(alumn|bacill|cact|foc|fung|nucle|radi|stimul|syllab|termin|vir)us$', 'gi' ), '$1i' ],
+
+		[ new RegExp( 'use$', 'gi' ), 'uses' ],
+
+		[ new RegExp( '(ax|cris|test)is$', 'gi' ), '$1es' ],
+
+		[ new RegExp( 's$', 'gi' ), 's' ],
+
+		[ new RegExp( '^$', 'gi' ), '' ],
+
+		[ new RegExp( '$', 'gi' ), 's' ]
 	],
 
-	'uninflected'	: [
+	'uninflected': [
 		'.*[nrlm]ese', '.*deer', '.*fish', '.*measles', '.*ois', '.*pox', '.*sheep', 'people'
 	],
 
-	'irregular'		: {
-		'atlas'			: 'altlases'
+	'irregular': {
+		'atlas': 'altlases'
 	}
 
 };
 
-var singular	= {
-
-	'rules'			: {
-		'/(s)tatuses$/i'	: '\1\2tatus'
-	},
-
-	'uninflected'	: [
-		'*.[nrlm]ese', '.*deer', '.*fish', '.*measles', '.*ios', '.*pox', '.*sheep', '.*ss'
-	],
-
-	'irregular'		: {
-		'foes'			: 'foe'
-	}
-
-};
-
-var uninflected	= [
-
-	'Amoyese'
-
-];
-
-var transliteration	= {
-
-	'/ä|æ|ǽ/' : 'ae'
-
-};
-
-var _cache			= [];
-var initialState	= [];
+var _cache = [];
+var initialState = [];
 
 function Inflector() {}
 
@@ -78,18 +75,16 @@ Inflector.prototype.pluralize = function( word ) {
 
 	var rules = plural.rules;
 	var match;
-	var found;
 
-	rules.forEach( function( value, index ) {
-		
-		match = word.match( value.regex );
-		if( match ) {
-			found = match[ 0 ].replace( value.regex, value.replacement );
+	for ( var i = 0; i < rules.length; i++ ) {
+
+		match = word.match( rules[ i ][ 0 ] );
+		if ( match ) {
+			_cache.pluralize = { word : word.replace( rules[ i ][ 0 ], rules[ i ][ 1 ] ) };
+			return _cache.pluralize.word;
 		}
 
-	} );
-
-	return found;
+	}
 
 };
 
--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 var Inflector = require( './Inflector' );
 
 var inflector = new Inflector();
-var plural = inflector.pluralize( 'mouse' );
-console.log( plural );
+
+var aWord =  [
+	inflector.pluralize( 'man' ),
+	inflector.pluralize( 'status' ),
+	inflector.pluralize( 'quiz'),
+	inflector.pluralize( 'ox' ),
+	inflector.pluralize( 'mouse' ),
+	inflector.pluralize( 'matrix' ),
+	inflector.pluralize( 'baby' ),
+	inflector.pluralize( 'hive' ),
+	inflector.pluralize( 'person' ),
+	inflector.pluralize( 'child' ),
+	inflector.pluralize( 'tomato' ),
+	inflector.pluralize( 'alumnus' ),
+	inflector.pluralize( 'use' ),
+	inflector.pluralize( 'axis' ),
+	inflector.pluralize( 'flag' )
+];
+
+console.log( aWord );
